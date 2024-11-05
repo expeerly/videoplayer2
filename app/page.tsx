b74bfb6d367bad3342157821d30f181d8589dc19
Ahmad Raza <vision.ahmad@gmail.com>
2024-11-05 19:58:51 +0500

    bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/component/ui/";
+import { Button } from "@/components/ui";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button size="sm">Button</Button>
+    <div className="flex items-center justify-center py-10">
+      <Button>
+        Button
+      </Button>
     </div>
   );
 }
