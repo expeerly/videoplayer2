526819f3570cab6aa405999eb57230fbea0643f2
Ahmad Raza <vision.ahmad@gmail.com>
2024-11-06 18:17:25 +0500

    fix bugs

--- components/BrandsSlider.tsx
+++ components/BrandsSlider.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CategorySlider from './Slider';
+import {Slider} from './Slider';
 
 function BrandsSlider() {
   return (
@@ -7,7 +7,7 @@ function BrandsSlider() {
     <div className='bg-[#4B49EB] max-w-[1170px] flex flex-col justify-center items-center  gap-4 py-20 px-5'>
        <h1 className='font-extrabold text-2xl text-white'>Video reviews on brands you love</h1>
        <p className='text-white mb-10'>Tap on brand logo to explore reviews.</p>
-       <CategorySlider />
+       <Slider />
        <button className=' bg-white mt-10 text-black font-bold rounded-full py-3 px-7'>Get Video Reviewed</button>
     </div>
     </div>
