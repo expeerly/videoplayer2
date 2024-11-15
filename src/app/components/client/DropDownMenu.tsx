96520510ca25170464ca9dd44e3491f969dc9c68
ahmadraza-gh <vision.ahmad@gmail.com>
2024-11-15 17:09:27 +0500

    scroll bug fix

--- src/app/components/client/DropDownMenu.tsx
+++ src/app/components/client/DropDownMenu.tsx
@@ -157,19 +157,20 @@ const DropDownMenuComponent: FunctionComponent<DropDownMenuProps> = ({
   }, [pathname]);
 
   useEffect(() => {
-    if (isOpen && window.innerWidth < 768) {
-      const scrollY = window.scrollY;
-      document.body.style.position = 'fixed';
-      document.body.style.width = '100%';
-      document.body.style.top = `-${scrollY}px`;
-    } else {
-      const scrollY = document.body.style.top;
-      document.body.style.position = '';
-      document.body.style.width = '';
-      document.body.style.top = '';
-      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    if (window.innerWidth < 768) {
+      if (isOpen) {
+        const scrollY = window.scrollY;
+        document.body.style.position = 'fixed';
+        document.body.style.width = '100%';
+        document.body.style.top = `-${scrollY}px`;
+      } else {
+        const scrollY = document.body.style.top;
+        document.body.style.position = '';
+        document.body.style.width = '';
+        document.body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+      }
     }
-
     return () => {
       document.body.style.position = '';
       document.body.style.width = '';
