b44ff8dcc65417fd89ffd8ad3aa71b104dd3deb9
Ali Ahmad <aliahmad.anwer@gmail.com>
2024-12-20 15:06:08 +0500

    IPhone video Loading fixed

--- src/app/components/client/VideoPlayer.tsx
+++ src/app/components/client/VideoPlayer.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { FunctionComponent, useEffect, useRef, useState } from 'react';
+import { FunctionComponent, useEffect, useRef } from 'react';
 import MuxPlayer, { MuxPlayerRefAttributes } from '@mux/mux-player-react';
 import { usePathname } from '@/src/i18n/routing';
-import { useTranslations } from 'next-intl';
 
 type Props = {
   playbackId: string;
@@ -12,24 +11,22 @@ type Props = {
 export const VideoPlayer: FunctionComponent<Props> = ({ playbackId }) => {
   const playerRef = useRef<MuxPlayerRefAttributes>(null);
   const containerRef = useRef<HTMLDivElement>(null);
-  const [canPlay, setCanPlay] = useState(false);
   const pathname = usePathname();
-  const t = useTranslations();
 
   useEffect(() => {
     if (!playerRef.current) return;
-    if (pathname.includes(`/explore/${playbackId}`) && canPlay) {
+    if (pathname.includes(`/explore/${playbackId}`)) {
       playerRef.current.play().catch(console.log);
     } else {
       playerRef.current.pause();
     }
-  }, [pathname, playbackId, canPlay]);
+  }, [pathname, playbackId]);
 
   return (
     <div ref={containerRef} className={`w-full h-full relative `}>
       <MuxPlayer
-        onCanPlay={() => setCanPlay(true)}
         ref={playerRef}
+        autoPlay
         loop
         muted={false}
         playbackId={playbackId}
@@ -41,11 +38,6 @@ export const VideoPlayer: FunctionComponent<Props> = ({ playbackId }) => {
         defaultHiddenCaptions={false}
         thumbnailTime={0}
       />
-      {canPlay === false && (
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  h-full w-full flex items-center justify-center bg-black">
-          {t('loading')}
-        </div>
-      )}
     </div>
   );
 };
