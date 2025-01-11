f01e8c538b7f07678656a8317dbf23fdc3c1bd2a
Ahmad Raza <vision.ahmad@gmail.com>
2025-01-11 15:13:46 +0500

    resolve vercel error

--- src/app/[locale]/[slug]/brand/page.tsx
+++ src/app/[locale]/[slug]/brand/page.tsx
@@ -11,11 +11,10 @@ import { LandingPageData } from '@/src/types';
 import { Languages } from '@/src/db/types';
 
 type PageProps = {
-  params: {
+  params: Promise<{
     locale: Languages;
     slug: string;
-  };
-  searchParams: { [key: string]: string | string[] | undefined };
+  }>;
 };
 
 export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
