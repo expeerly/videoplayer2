05958278e0bae6128d625610ec38c0147ff15fe8
Kenneth Aladi <kendevops@gmail.com>
2026-01-14 13:42:50 +0100

    commented out Categories from homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from '../components/server/HeroSection';
 import { ExploreReviewers } from '../components/server/ExploreReviewers';
 import { HowExpeerlyWorks } from '../components/server/HowExpeerlyWork';
 import { BrandsSlider } from '../components/server/BrandsSlider';
-import { CategoriesSlider } from '../components/server/CategoriesSlider';
+// import { CategoriesSlider } from '../components/server/CategoriesSlider';
 import { ConversionSlider } from '../components/server/Conversion';
 import { NextPage, Metadata } from 'next';
 import { getDictionary } from '../../lib/dictionary';
@@ -63,7 +63,7 @@ const HomePage: NextPage<PageProps> = async ({ params }) => {
           }
         />
       </section>
-      <CategoriesSlider locale={locale} />
+      {/* <CategoriesSlider locale={locale} /> */}
       <section className="flex justify-center max-w-[900px] w-full mx-auto pb-12 mt-5 md:pb-[70px]  ">
         <ReviewGridSection
           getGridVideos={() =>
